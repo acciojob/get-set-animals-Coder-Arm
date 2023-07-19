9922c2b4b0f18dede7a9ecc214d6d7fd1484dc7b
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-19 12:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this.species = species;
+		this._species = species;
 	}
 	get species(){
-		return this.species;
+		return this._species;
 	}
 	set species(val){
-		return this.species = val;
+		return this._species = val;
 	}
 	makeSound(){
-		console.log(`The ${this.species} makes a sound`);
+		console.log(`The ${this._species} makes a sound`);
 	}
 }
 
